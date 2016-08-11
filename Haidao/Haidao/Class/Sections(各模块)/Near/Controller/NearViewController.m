c80e265fe956078d13380c7fcca371ab7de0a22d
sugeMarket <1366225686@qq.com>
2016-08-11 17:59:08 +0800

    FR
    change

--- Haidao/Haidao/Class/Sections(各模块)/Near/Controller/NearViewController.m
+++ Haidao/Haidao/Class/Sections(各模块)/Near/Controller/NearViewController.m
@@ -13,12 +13,15 @@
 #import "BusinessTypeButton.h"
 #import "NearListViewController.h"
 
+#import "PositionBtn.h"
+#import "SearchViewController.h"
+
 /** 顶部商家分类的宽高比 */
 static CGFloat const NearBusinessTypeCollectionViewWH = 0.55;
 /**  商家分类CollectionViewCell标记  */
 static NSString * const BusinessTypeviewCellID = @"BusinessTypeviewCell";
 
-@interface NearViewController ()<UITableViewDelegate,UITableViewDataSource,UICollectionViewDataSource,UICollectionViewDelegate>
+@interface NearViewController ()<UITableViewDelegate,UITableViewDataSource,UICollectionViewDataSource,UICollectionViewDelegate,UITextFieldDelegate>
 
 /**  商家列表  */
 @property (nonatomic, strong) UITableView *tableview;
@@ -34,6 +37,9 @@ static NSString * const BusinessTypeviewCellID = @"BusinessTypeviewCell";
 /**  商家分类页数  */
 @property (nonatomic, assign) NSInteger typePages;
 
+/** 定位按钮  */
+@property (nonatomic, weak) UIButton *positionBtn;
+
 
 @end
 
@@ -45,6 +51,8 @@ static NSString * const BusinessTypeviewCellID = @"BusinessTypeviewCell";
     [self.nearBusinessArray addObjectsFromArray:@[@"",@"",@"",@"",@"",@"",@""]];
     
     [self setTableHeaderView];
+    
+    [self addNavigationItem];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -55,6 +63,9 @@ static NSString * const BusinessTypeviewCellID = @"BusinessTypeviewCell";
     
     self.nearBusinessTypeCollectionView.delegate = self;
     self.nearBusinessTypeCollectionView.dataSource = self;
+    
+    // 导航栏颜色
+    self.navigationController.navigationBar.barTintColor = kSubjectColor;
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
@@ -79,6 +90,59 @@ static NSString * const BusinessTypeviewCellID = @"BusinessTypeviewCell";
     
     if (_typeArray != nil) _typeArray = nil;
 
+}
+
+- (void)addNavigationItem {
+    
+    //    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]init];
+    //搜索
+    CGRect searchTextFieldF = CGRectMake(0, 0, kScreenWidth - 120, 30);
+    UITextField *searchTextField = [[UITextField alloc] initWithFrame:searchTextFieldF];
+    searchTextField.backgroundColor = [UIColor whiteColor];
+    UIImageView *fieldLeftView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 30, 30)];
+    fieldLeftView.image = [UIImage imageNamed:@"搜索"];
+    searchTextField.leftView = fieldLeftView;
+    searchTextField.leftViewMode = UITextFieldViewModeAlways;//一直显示
+    
+    searchTextField.font = kFontSize(14);
+    searchTextField.placeholder = @"请输入商家名或分类";
+    searchTextField.delegate = self;
+    [searchTextField.layer setMasksToBounds:YES];
+    searchTextField.layer.cornerRadius = 15;
+    self.navigationItem.titleView = searchTextField;
+    
+    PositionBtn *positionBtn = [[PositionBtn alloc]initWithFrame:CGRectMake(0, 0, 56, 30)];
+    positionBtn.titleLabel.font = kFontSize(14);
+    positionBtn.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [positionBtn setTitle:@"北京市" forState:UIControlStateNormal];
+    [positionBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [positionBtn setTitleColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
+    [positionBtn setImage:[UIImage imageNamed:@"下拉箭头"] forState:UIControlStateNormal];
+    _positionBtn = positionBtn;
+    
+    [positionBtn addTarget:self action:@selector(positionBtnAction:) forControlEvents:UIControlEventTouchUpInside];
+    
+    UIView *matginView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 1, 20)];
+    
+    UIBarButtonItem *positionItem = [[UIBarButtonItem alloc]initWithCustomView:positionBtn];
+    UIBarButtonItem *marginItem = [[UIBarButtonItem alloc]initWithCustomView:matginView];
+    
+    self.navigationItem.rightBarButtonItems = @[positionItem,marginItem];
+}
+
+/**
+ *  点击右上方定位按钮
+ */
+- (void)positionBtnAction:(UIButton *)button {
+    
+}
+
+#pragma mark - UITextFieldDelegate
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    [textField resignFirstResponder];
+    NSLog(@"跳转到搜索页面");
+    SearchViewController *searchVC = [[SearchViewController alloc]init];
+    [self.navigationController pushViewController:searchVC animated:YES];
 }
 
 /**
